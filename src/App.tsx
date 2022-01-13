c294068c1cac26802cc935f709b3481b12ec426f
abddulrahmaanHussein <a.hussein@fis.ps>
2022-01-13 12:25:20 +0200

    Creating Snackbar component using react-toastify library

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,15 @@ import Loading from "./components/Loading/loading";
 import { AppState } from "./redux/store";
 import { toggleTheme } from "./redux/actions/theme.action";
 import RootRoutes from "./routes/RootRoutes";
+import { Button } from "./components/Button/Button.style";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const dispatch = useDispatch();
+
   const { theme } = useSelector((state: AppState) => state);
+
   const toggleTheme_ = useCallback(() => {
     dispatch(toggleTheme(theme === "light" ? "dark" : "light"));
   }, [theme]);
@@ -17,12 +22,16 @@ function App() {
   return (
     <ThemeProvider theme={{}}>
       <Suspense fallback={<Loading />}>
+        <Button onClick={toggleTheme_} height="60px" width="300px">
+          click
+        </Button>
         <Routes>
           {RootRoutes.routes.map((route, Index) => (
             <Route path={route.path} element={route.component} key={Index} />
           ))}
         </Routes>
       </Suspense>
+      <ToastContainer autoClose={3000} />
     </ThemeProvider>
   );
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,41 +1,39 @@
-import { createTheme,Theme,ThemeOptions } from "@mui/material/styles";
+import { createTheme, Theme, ThemeOptions } from "@mui/material/styles";
 
-const baseTheme:ThemeOptions =createTheme({
-  typography:{
-    fontFamily:'Mulish, sans-serif',
-  }
-})
-const lighttheme:Theme = createTheme({
+const baseTheme: ThemeOptions = createTheme({
+  typography: {
+    fontFamily: "Mulish, sans-serif",
+  },
+});
+const lighttheme: Theme = createTheme({
   ...baseTheme,
-   palette:{
-     mode:'light',
-     primary:{
-       main:'',
-       light:'',
-       dark:'',
-     },
-     secondary:{
-       main:'',
-       light:'',
-       dark:'',
-
-     },
-     error:{
-      main:"",
-      light:'',
-      dark:'',
-     }
-   }
-  });
-  const darkTheme:Theme=createTheme({
-    ...baseTheme,
-   palette:{
-     mode:'dark',
-     primary:{
-       main:'',
-       light:'',
-       dark:'',
-    
-     }
-   }
-  })
+  palette: {
+    mode: "light",
+    primary: {
+      main: "",
+      light: "",
+      dark: "",
+    },
+    secondary: {
+      main: "",
+      light: "",
+      dark: "",
+    },
+    error: {
+      main: "",
+      light: "",
+      dark: "",
+    },
+  },
+});
+const darkTheme: Theme = createTheme({
+  ...baseTheme,
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "",
+      light: "",
+      dark: "",
+    },
+  },
+});
