68a5e3906182ca94b739e6e33277eb0a2f58444e
devmohammedna <ma7mmmed.n3im@gmail.com>
2022-01-13 13:50:23 +0200

    add theme font and color in darktheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { Suspense, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "@mui/material/styles";
 import Loading from "./components/Loading/loading";
 import { AppState } from "./redux/store";
 import { toggleTheme } from "./redux/actions/theme.action";
+import { lightTheme, darkTheme } from "./theme/theme";
 import RootRoutes from "./routes/RootRoutes";
+import { Button } from "./components/Button/Button.style";
 
 function App() {
   const dispatch = useDispatch();
   const { theme } = useSelector((state: AppState) => state);
+
   const toggleTheme_ = useCallback(() => {
     dispatch(toggleTheme(theme === "light" ? "dark" : "light"));
   }, [theme]);
 
   return (
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <Suspense fallback={<Loading />}>
         <Routes>
           {RootRoutes.routes.map((route, Index) => (
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,41 +1,82 @@
-import { createTheme,Theme,ThemeOptions } from "@mui/material/styles";
+import { createTheme, Theme, ThemeOptions } from "@mui/material/styles";
 
-const baseTheme:ThemeOptions =createTheme({
-  typography:{
-    fontFamily:'Mulish, sans-serif',
-  }
-})
-const lighttheme:Theme = createTheme({
+const baseTheme: ThemeOptions = createTheme({
+  typography: {
+    fontFamily: "Mulish, sans-serif",
+    fontSize: 14,
+    h1: {
+      fontSize: "3.75rem",
+      lineHeight: 1.167,
+      letterSpacing: "-0.01562em",
+      fontWeight: "bold",
+    },
+    h2: {
+      fontSize: "2rem",
+      fontWeight: "bold",
+      lineHeight: 1.2,
+      letterSpacing: "-0.01562em",
+    },
+    h3: {
+      fontSize: "2rem",
+      fontWeight: "normal",
+      lineHeight: 1.167,
+      letterSpacing: "-0.01562em",
+    },
+    h4: {
+      fontSize: "1.875rem",
+      lineHeight: 1.235,
+      letterSpacing: "-0.01562em",
+    },
+    button: {
+      fontSize: "22px",
+      lineHeight: 1.167,
+      fontWeight: "normal",
+      textTransform: "uppercase",
+      letterSpacing: "-0.01562em",
+    },
+  },
+});
+export const lightTheme: Theme = createTheme({
   ...baseTheme,
-   palette:{
-     mode:'light',
-     primary:{
-       main:'',
-       light:'',
-       dark:'',
-     },
-     secondary:{
-       main:'',
-       light:'',
-       dark:'',
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#fcdd06",
+    },
+    secondary: {
+      main: "#f2f2f2",
+    },
 
-     },
-     error:{
-      main:"",
-      light:'',
-      dark:'',
-     }
-   }
-  });
-  const darkTheme:Theme=createTheme({
-    ...baseTheme,
-   palette:{
-     mode:'dark',
-     primary:{
-       main:'',
-       light:'',
-       dark:'',
-    
-     }
-   }
-  })
+    text: {
+      primary: "#242424",
+      secondary: "#707070",
+      disabled: "#979797",
+    },
+    background: {
+      paper: "#f7f8fc",
+      default: "#ffffff",
+    },
+  },
+});
+export const darkTheme: Theme = createTheme({
+  ...baseTheme,
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#fcdd06",
+    },
+    secondary: {
+      main: "#2f2f2f",
+    },
+
+    text: {
+      primary: "#ffffff",
+      secondary: "#f7f8fc",
+      disabled: "rgba(255,255,255,0.5)",
+    },
+    background: {
+      paper: "#282c34",
+      default: "#282c34",
+    },
+  },
+});
