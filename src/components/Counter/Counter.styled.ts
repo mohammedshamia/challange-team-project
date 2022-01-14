9b7ff4b23f0d83badd428542f0ce770e7b7f5ce0
hadeel480 <ansam12290@gmail.com>
2022-01-14 04:30:21 +0300

    update on counter style  component

--- src/components/Counter/Counter.styled.ts
+++ src/components/Counter/Counter.styled.ts
@@ -9,13 +9,18 @@ export const CounterContainer = styled('div')(({ theme }) => ({
   
 
   export const H1Style = styled('h1')(({ theme }) => ({
-    margin:' 10px 20px 10px 20px',
-    width:' 14px',
-    height:' 30px',
+    width:' 204px',
+    height:' 40px',
     font: 'normal normal bold 24px/14px Muli',
     color:' #242424',
     letterSpacing: '0.48px',
-    textAlign: 'left'
+    textAlign: 'left',
+    border:' 1px solid #F2F2F2',
+    padding:'15px 100px',
+    boxSizing:'border-box',
+    marginTop:'0'
+
+
 
     }));
 
