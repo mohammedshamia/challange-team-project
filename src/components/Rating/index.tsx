8e525c2de93877fa427d2becb0367880d823fd5c
hadeel480 <ansam12290@gmail.com>
2022-01-15 10:30:20 +0300

    update on rating component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,16 +1,16 @@
 import Rating from "@mui/material/Rating";
 import {  useEffect,useState } from "react";
-import { CounterRating } from "./Rating.styled";
+import { RatingContiner } from "./Rating.styled";
 
 
 
 interface Iprops {
   value: number;
-  precision: number;
-  isReadOnly: boolean;
-  isDisabled: boolean;
-  name: string;
-  onChangeValue:Function;
+  precision?: number;
+  isReadOnly?: boolean;
+  isDisabled?: boolean;
+  name?: string;
+  onChangeValue?:Function;
   paddingRating?:string;
 }
 
@@ -30,11 +30,11 @@ function RatingComponent({
   }
 
   useEffect(() => {
-    onChangeValue(valueRating)
+    onChangeValue?.(valueRating)
   }, [valueRating])
 
   return (
-    <CounterRating padding={paddingRating}>
+    <RatingContiner padding={paddingRating}>
       <Rating
       name={name}
       value={valueRating}
@@ -44,7 +44,7 @@ function RatingComponent({
       onChange={(event, newValue) => {
         handleChangeRating(newValue as number)      }}
        />
-    </CounterRating>
+    </RatingContiner>
  
   );
 }
