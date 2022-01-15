8a5439418bb5a458f76ed87229691d6f142eb07f
devmohammedna <ma7mmmed.n3im@gmail.com>
2022-01-15 11:08:31 +0200

    add mui provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,36 @@ import { Suspense, useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { ThemeProvider as MuiTheme } from "@mui/material/styles";
 import Loading from "./components/Loading/loading";
 import { AppState } from "./redux/store";
-import { toggleTheme } from "./redux/actions/theme.action";
+// import { toggleTheme } from "./redux/actions/theme.action";
 import { lightTheme, darkTheme } from "./theme/theme";
 import RootRoutes from "./routes/RootRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
-
 
 function App() {
   const dispatch = useDispatch();
 
   const { theme } = useSelector((state: AppState) => state);
 
-  const toggleTheme_ = useCallback(() => {
-    dispatch(toggleTheme(theme === "light" ? "dark" : "light"));
-  }, [theme, dispatch]);
+  // const toggleTheme_ = useCallback(() => {
+  //   dispatch(toggleTheme(theme === "light" ? "dark" : "light"));
+  // }, [theme, dispatch]);
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          {RootRoutes.routes.map((route, Index) => (
-            <Route path={route.path} element={route.component} key={Index} />
-          ))}
-        </Routes>
-      </Suspense>
-      <ToastContainer autoClose={3000} />
+      <MuiTheme theme={theme === "light" ? lightTheme : darkTheme}>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            {RootRoutes.routes.map((route, Index) => (
+              <Route path={route.path} element={route.component} key={Index} />
+            ))}
+          </Routes>
+        </Suspense>
+        <ToastContainer autoClose={3000} />
+      </MuiTheme>
     </ThemeProvider>
   );
 }
