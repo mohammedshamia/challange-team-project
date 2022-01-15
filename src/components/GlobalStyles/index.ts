b3ae1bbc959d95070450556587267b1d466308c6
abddulrahmaanHussein <a.hussein@fis.ps>
2022-01-15 14:51:00 +0200

    Fix Global Style

---
+++ src/components/GlobalStyles/index.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    padding:0;
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+`;
--- src/screens/registration/signIn/index.tsx
+++ src/screens/registration/signIn/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-function SignIn(){
-    return(
-        <p>SignIn </p>
-    )
-}
+import { Formik, Form } from "formik";
+import { Container } from "./SignIn.styled";
+import { Typography } from "@mui/material";
 
-export default SignIn as React.FC;
+const SignIn = () => {
+  return (
+    <Container>
+      <div>
+        <Typography variant="h1">Login.</Typography>
+        <Typography variant="h4">
+          Login with your data that you entered during registration
+        </Typography>
+      </div>
+      <img src={"/static/SignIn.png"} alt={"login pic"} />
+    </Container>
+  );
+};
+
+export default SignIn;
