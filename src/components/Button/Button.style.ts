0ae72bc52e1ca6ea9072b442d937ed3b65deee20
ferassalhelo <feras.s.alhelo@gmail.com>
2022-01-15 12:46:21 +0200

    edit button-1

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,19 +1,20 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import {Button as B } from '@mui/material'
 
-interface Iprpos {
-  width: string;
-  height: string;
-  children: string | ReactNode;
+ interface Iprpos    {
+  width: string| number;
+    children: string | ReactNode;
+  height?: string| number;
   borderRadius?: string | undefined;
   color?: string;
   backgroundColor?: string;
   fontSize?: string;
 }
 
-export const Button = styled.button<Iprpos>((props: Iprpos) => ({
-  width: props.width,
-  height: props.height,
+export const Button = styled(B)<Iprpos>((props: Iprpos) => ({
+  width: Number(props.width) ? props.width + 'px' : props.width,
+  height:  Number(props.height) ? props.height + 'px' : props.height|| '40px',
   color: props.color || "#000000",
   backgroundColor: props.backgroundColor || "#FCDD06",
   borderRadius: props.borderRadius || "10px",
