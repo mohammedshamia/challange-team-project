46347505934e445ba2b9a749386f8939150271c0
devmohammedna <ma7mmmed.n3im@gmail.com>
2022-01-15 21:21:15 +0200

    edit some line theme in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import { Suspense, useCallback, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { Suspense } from "react";
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Loading from "./components/Loading/loading";
 import { AppState } from "./redux/store";
-import { toggleTheme } from "./redux/actions/theme.action";
 import { lightTheme, darkTheme } from "./theme/theme";
 import RootRoutes from "./routes/RootRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-
-
 function App() {
-  const dispatch = useDispatch();
-
   const { theme } = useSelector((state: AppState) => state);
-
-  const toggleTheme_ = useCallback(() => {
-    dispatch(toggleTheme(theme === "light" ? "dark" : "light"));
-  }, [theme, dispatch]);
-
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <Suspense fallback={<Loading />}>
