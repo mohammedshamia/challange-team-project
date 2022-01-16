1ced2dedbd23a409dddf623dfe14ea613904d1fa
hadeel480 <ansam12290@gmail.com>
2022-01-16 20:32:26 +0300

    update on counter comonent

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -3,6 +3,7 @@ import { Button } from '../Button/Button.style';
 import { CounterContainer, H1Style } from './Counter.styled';
 import AddIcon from '@mui/icons-material/Add';
 import MaximizeIcon from '@mui/icons-material/Maximize';
+import { Typography } from '@mui/material';
 
  function Counter({value, onChange,maxValue,minValue}:{value:number, minValue:number,maxValue:number,onChange: Function}) {
     const [counter, setCounter] = useState<number>(value);
@@ -23,16 +24,17 @@ import MaximizeIcon from '@mui/icons-material/Maximize';
          border= '1px solid #FCDD06'
          borderRadius='0'
          color='gray'
-         onClick={()=>{handleChangeCounter('increment')}}   max={maxValue}>
+         onClick={()=>{handleChangeCounter('increment')}}    >
           <AddIcon/>
         </Button>
-        <H1Style>{counter}</H1Style>
-        <Button width= '40PX' height='40px'  backgroundColor='white'  
+        <Typography variant="h3" color="text-primary"  sx={{width:'100px', border:'1px solid #F7F8FC',paddingLeft:'50px',boxSizing:'border-box'}}> {counter}</Typography>
+        <Button width= '40PX' height='40px'
+          backgroundColor='white'  
            border= '1px solid #FCDD06'
            borderRadius='0'
            color='#000000' 
 
-          onClick={()=>{handleChangeCounter('decrement')}} min={minValue} >
+          onClick={()=>{handleChangeCounter('decrement')}}  >
           <MaximizeIcon/>
         </Button>
      </CounterContainer>
