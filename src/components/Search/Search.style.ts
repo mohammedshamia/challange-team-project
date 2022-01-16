ab0eed1c27626c073432893419aca8672509b483
ferassalhelo <feras.s.alhelo@gmail.com>
2022-01-17 00:19:09 +0200

    header finish

--- src/components/Search/Search.style.ts
+++ src/components/Search/Search.style.ts
@@ -1,21 +1,27 @@
 import styled from "styled-components";
 
-export const Lable = styled.div`
-  background-color: white;
-  border-radius: 6px;
-  width: 705px;
-  height: 40px;
-  display: flex;
-`;
+interface Iprops{
+  width?:string;
+}
+
+export const Lable = styled.label<Iprops>((props:Iprops) => ({
+  backgroundColor: 'white',
+  borderRadius: '6px',
+  width: props.width || '43%',
+  height: '40px',
+  display: 'flex',
+}));
+
 
 export const SearchInput = styled.input.attrs({ type: "search" })`
   border: 0;
   outline: 0;
   border-radius: 6px;
   background-color: transparent;
-  width: 553px;
+  width: 80%;
   height: 40px;
   padding: 0 26px;
   color: #242424;
   font-size: 13px;
+
 `;
--- src/components/common/Header/Header.style.ts
+++ src/components/common/Header/Header.style.ts
@@ -1,12 +1,33 @@
-import styled from "@emotion/styled";
-export const HeaderBox = styled.div`
+import { Box, IconButton, Toolbar as T } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const HeaderBox = styled('div')`
   position: sticky;
   top: 0;
   height: 85px;
-  padding: 0 140px;
+  padding: 0 7%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   background-color: #242424;
   color: white;
 `;
+
+
+export const ButtonIcon = styled(IconButton)(({ theme }) => ({
+  borderRadius: "7px",
+  border: "1px solid #707070",
+  color: "#fff",
+  marginLeft:'10px'
+}));
+
+export const Toolbar = styled(T)(() => ({
+flexGrow:'99'
+}));
+
+
+export const FlexBox = styled(Box)(()=>({
+  display:'flex',
+    
+    
+}))
