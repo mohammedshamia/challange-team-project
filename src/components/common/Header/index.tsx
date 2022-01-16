e689232350001aaff988b2089f2994a292407ae6
ferassalhelo <feras.s.alhelo@gmail.com>
2022-01-16 22:00:10 +0200

    edit on header

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,14 +1,93 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
 import Logo from "../../Logo";
-import Search from "../../Search";
-import { HeaderBox } from "./Header.style";
+import Search from "../../Search/index";
 import BottonNavigationGroup from "../../ButtomNavigation";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "../../../redux/store";
+import { toggleTheme } from "../../../redux/actions/theme.action";
+import { ButtonIcon, HeaderBox } from "./Header.style";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+
 export default function Header() {
+  const dispatch = useDispatch();
+  const { theme } = useSelector((state: AppState) => state);
+
+  const toggleTheme_ = React.useCallback(() => {
+    dispatch(toggleTheme(theme === "light" ? "dark" : "light"));
+  }, [theme, dispatch]);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <HeaderBox>
-      <Logo />
-      <Search />
-      <BottonNavigationGroup cartItems={2} wishlist={3} />
+      <Toolbar disableGutters>
+        <Box sx={{ flexGrow: "99", display: { xs: "block", lg: "none" } }}>
+          <Logo />
+        </Box>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", lg: "none" } }}>
+          <IconButton size="large" onClick={handleOpenNavMenu} color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", lg: "none" },
+            }}
+          >
+            <Search />
+            <BottonNavigationGroup wishlist={9} cartItems={98} />
+            <ButtonIcon onClick={toggleTheme_} aria-label="toggle-theme">
+              {theme === "light" ? <Brightness4Icon /> : <Brightness7Icon />}
+            </ButtonIcon>
+          </Menu>
+        </Box>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: {
+              xs: "none",
+              lg: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            },
+          }}
+        >
+          <Logo />
+          <Search />
+          <BottonNavigationGroup wishlist={9} cartItems={98} />
+          <ButtonIcon onClick={toggleTheme_} aria-label="toggle-theme">
+            {theme === "light" ? <Brightness4Icon /> : <Brightness7Icon />}
+          </ButtonIcon>
+        </Box>
+      </Toolbar>
     </HeaderBox>
   );
 }
