1178d8106dcd2a401664094e87a1ef0f5c7b2003
hadeel480 <ansam12290@gmail.com>
2022-01-17 17:11:52 +0300

    Update rating component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,8 +1,6 @@
 import Rating from "@mui/material/Rating";
-import {  useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { RatingContiner } from "./Rating.styled";
-
-
 
 interface Iprops {
   value: number;
@@ -10,8 +8,8 @@ interface Iprops {
   isReadOnly?: boolean;
   isDisabled?: boolean;
   name?: string;
-  onChangeValue?:Function;
-  paddingRating?:string;
+  onChangeValue?: Function;
+  paddingRating?: string;
 }
 
 function RatingComponent({
@@ -21,32 +19,30 @@ function RatingComponent({
   isDisabled,
   name,
   onChangeValue,
-  paddingRating
+  paddingRating,
 }: Iprops) {
   const [valueRating, setvalueRating] = useState<number>(value);
-  const handleChangeRating=(newValue : number)=>{
-    setvalueRating(newValue );
-
-  }
+  const handleChangeRating = (newValue: number) => {
+    setvalueRating(newValue);
+  };
 
   useEffect(() => {
-    onChangeValue?.(valueRating)
-  }, [valueRating])
+    onChangeValue?.(valueRating);
+  }, [valueRating]);
 
   return (
     <RatingContiner padding={paddingRating}>
       <Rating
-      name={name}
-      value={valueRating}
-      precision={precision}
-      readOnly={isReadOnly}
-      disabled={isDisabled}
-      onChange={(event, newValue) => {
-        handleChangeRating(newValue as number)      }}
-       />
+        name={name}
+        value={valueRating}
+        precision={precision}
+        readOnly={isReadOnly}
+        disabled={isDisabled}
+        onChange={(event, newValue) => {
+          handleChangeRating(newValue as number);
+        }}
+      />
     </RatingContiner>
- 
   );
 }
 export default RatingComponent;
-
