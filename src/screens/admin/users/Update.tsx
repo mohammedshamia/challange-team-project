f4aeae5b75b37c8efe121ec00500b79695617771
reeham_raed <rolla.ghanima@gmail.com>
2022-01-24 15:36:52 +0200

    add cellRenderer for user actions

---
+++ src/screens/admin/users/Update.tsx
@@ -0,0 +1,23 @@
+import { ICellRendererParams } from "ag-grid-community";
+import { Icon } from "@mui/material";
+import { Link } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
+
+// import { useDispatch } from "react-redux";
+
+const Edit = (props: ICellRendererParams) => {
+    // const dispatch = useDispatch();
+    console.log(props,'props1')
+    return (
+        <>
+            <Link to={`/users/${props.data._id}`}>
+                <Icon>
+                    <EditIcon />
+                </Icon>
+            </Link>
+
+        </>
+    );
+};
+
+export default Edit;
--- src/screens/admin/users/index.tsx
+++ src/screens/admin/users/index.tsx
@@ -6,28 +6,39 @@ import {useEffect} from "react";
 import { getUsers} from "../../../redux/actions/user.actions";
 import {useDispatch, useSelector} from "react-redux";
 import {AppState} from "../../../redux/store";
+import Update from "./Update";
 
 const columns: IColumn[] = [
 
   {
     name: "firstName",
+      cellRenderer: (params) => `${params.data?.firstName}`,
+
   },
   {
     name: "lastName",
+    cellRenderer: (params) => `${params.data?.lastName}`,
   }
   ,{
     name: "email",
+      cellRenderer: (params) => `${params.data?.email}`,
   }
-  ,{
-    name: "password",
-  }
+  // ,{
+  //   name: "password",
+  //     cellRenderer: (params) => `${params.data?.password}`,
+  // }
   ,{
     name: "isAdmin",
+      cellRenderer: (params) => `${params.data?.isAdmin}`,
   },
   {
     name: "dateOfBirth",
+    cellRenderer: (params) => `${params.data?.dateOfBirth}`,
   },
-
+  {
+    name: "actions",
+    cellRenderer: "ActionsRenderer",
+  },
 
 ];
 
@@ -44,14 +55,17 @@ function Users () {
       }, [dispatch]);
 
 
-  console.log(users,'users')
   return (
          <Container>
           <Typography variant="h2" color="text.primary">
             Users
           </Typography>
           <div style={{ width: "100%", margin: "auto" }}>
-            <Table data={users} columns={columns} />
+            <Table data={users} columns={columns}
+                   frameworkComponents={{
+                     ActionsRenderer: Update,
+                   }}
+             />
           </div>
       </Container>
 
