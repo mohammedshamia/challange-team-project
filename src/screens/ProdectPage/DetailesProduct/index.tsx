61382c27f7e0d2ab5ddec5e8bbc10ab1c39ccb52
ferassalhelo <feras.s.alhelo@gmail.com>
2022-01-26 01:05:15 +0200

    edit the small imges (sizes, responsiv, display)

--- src/screens/ProdectPage/DetailesProduct/index.tsx
+++ src/screens/ProdectPage/DetailesProduct/index.tsx
@@ -79,7 +79,12 @@ export default function DetailesProdect({
     <div>
       <DetailesProductContainer>
         <ImagesContiner>
-          <img src={imgpropdect} alt={nameProdect} width="90%" />
+          <img
+            src={imgpropdect}
+            alt={nameProdect}
+            width="90%"
+            style={{ objectFit: "contain" }}
+          />
           <SizesOFimgContiner>
             {img1propdect && (
               <img
@@ -87,6 +92,7 @@ export default function DetailesProdect({
                 alt="nameprodect"
                 width="29%"
                 height={"100%"}
+                style={{ objectFit: "contain" }}
               />
             )}
             {img2propdect && (
@@ -95,6 +101,7 @@ export default function DetailesProdect({
                 alt="nameprodect"
                 width="29%"
                 height={"100%"}
+                style={{ objectFit: "contain" }}
               />
             )}
             {img3propdect && (
@@ -103,6 +110,7 @@ export default function DetailesProdect({
                 alt="nameprodect"
                 width="29%"
                 height={"100%"}
+                style={{ objectFit: "contain" }}
               />
             )}
           </SizesOFimgContiner>
