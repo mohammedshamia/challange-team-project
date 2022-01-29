d277a5d3f2eea337860f5e1c70d9b45a098df4b7
devmohammedna <ma7mmmed.n3im@gmail.com>
2022-01-29 13:24:14 +0200

    create details order page

--- src/screens/admin/orders/OrderDetails.tsx
+++ src/screens/admin/orders/OrderDetails.tsx
@@ -1,10 +1,26 @@
+import { ICellRendererParams } from "ag-grid-community";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { AppState } from "../../../redux/store";
+import Order from "../../payment/OrderDetails";
+const OrderDetails = ({ params }: { params: ICellRendererParams }) => {
+  // const params = useParams();
+  const {
+    orders: {
+     
+    },
+  } = useSelector((state: AppState) => state);
+  const dispatch = useDispatch();
 
-const OrderDetails = () => {
-  const params = useParams();
-  useEffect(() => {}, [params]);
-  return <></>;
+ 
+  useEffect(() => {
+  
+      setOrder();
+
+    console.log(params.data);
+  }, []);
+  return <>{/* <Order products={cart.items} cart={cart} /> */}</>;
 };
 
 export default OrderDetails;
